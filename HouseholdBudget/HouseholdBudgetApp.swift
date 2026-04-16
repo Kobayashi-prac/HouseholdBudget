9f54295c4ebe0e2d5039631500cbd45f3863ed0e
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-04-17 02:29:44 +0900

    First Commit

---
+++ HouseholdBudget/HouseholdBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseholdBudgetApp.swift
+//  HouseholdBudget
+//
+//  Created by swiftUI on 2026/04/17.
+//
+
+import SwiftUI
+
+@main
+struct HouseholdBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HouseholdBudgetTests/HouseholdBudgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  HouseholdBudgetTests.swift
+//  HouseholdBudgetTests
+//
+//  Created by swiftUI on 2026/04/17.
+//
+
+import Testing
+@testable import HouseholdBudget
+
+struct HouseholdBudgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
