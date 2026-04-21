6226c270f4c098d3ddb9c1e33d324a1350da0ef0
Kobayashi-prac <df2jm2278f1p6@gmail.com>
2026-04-22 01:09:29 +0900

    SwiftUIについてキャッチアップを一旦プッシュ

---
+++ HouseholdBudget/Household/View/CameraScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CameraScreen.swift
+//  HouseholdBudget
+//
+//  Created by swiftUI on 2026/04/17.
+//
+
+import SwiftUI
+
+struct CameraScreen: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    CameraScreen()
+}
---
+++ HouseholdBudget/StockSimulator/View/StockSimulatorScreen.swift
@@ -0,0 +1,17 @@
+//
+//  StockSimulatorScreen.swift
+//  HouseholdBudget
+//
+//  Created by swiftUI on 2026/04/19.
+//
+
+import SwiftUI
+
+struct StockSimulatorScreen: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    StockSimulatorScreen()
+}
